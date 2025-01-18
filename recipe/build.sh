d9df67b1c311c766c726bd94edf1883f0c217a3b
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 12:30:05 -0800

    fix Rscript path

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
 export DISABLE_AUTOBREW=1
-${R} CMD INSTALL --build . ${R_ARGS}
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+    sed -i "s?\$(R_HOME)?${BUILD_PREFIX}?g" src/Makevars
+    sed -i "s/'cat(\"-I\", R.home(\"include\"), sep=\"\")'/'cat(\"-I\", R.home(\"include\"), sep=\"\")' | sed -e 's|${BUILD_PREFIX}|${PREFIX}|g' | sed -e 's/WARNING: ignoring environment value of R_HOME//g'/" src/Makevars
+fi
+
+${R} CMD INSTALL --build . ${R_ARGS:-}
