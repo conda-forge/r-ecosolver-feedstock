e2ba0a86b7d49a9b7137e5624efc188af54338f6
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 13:09:18 -0800

    fix sed statement

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export DISABLE_AUTOBREW=1
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     sed -i "s?\$(R_HOME)?${BUILD_PREFIX}?g" src/Makevars
-    sed -i "s/'cat(\"-I\", R.home(\"include\"), sep=\"\")'/'cat(\"-I\", R.home(\"include\"), sep=\"\")' | sed -e 's|${BUILD_PREFIX}|${PREFIX}|g' | sed -e 's/WARNING: ignoring environment value of R_HOME//g'/" src/Makevars
+    sed -i "s/'cat(\"-I\", R.home(\"include\"), sep=\"\")'/'cat(\"-I\", R.home(\"include\"), sep=\"\")' | sed -e 's?${BUILD_PREFIX}?${PREFIX}?g' | sed -e 's?WARNING: ignoring environment value of R_HOME??g'/" src/Makevars
 fi
 
 ${R} CMD INSTALL --build . ${R_ARGS:-}
